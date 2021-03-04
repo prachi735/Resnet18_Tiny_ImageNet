5a542e24c5a586ea6f76656a605ca7f87e3b94ab
Prachi735 <Prachi735@gmail.com>
2021-03-04 10:04:19 +0530

    corrected image display in all methods

--- utils.py
+++ utils.py
@@ -44,11 +44,11 @@ def get_misclassified_images(gbn_model, test_loader):
 def plot_results(train_losses, train_acc, test_losses, test_acc):
     data = {'train_loss': train_losses,  'train_acc': train_acc,
             'test_loss': test_losses,  'test_acc': test_acc}
-    fig, axs = plt.subplots(1, 4, figsize=(30, 5))
+    _, axs = plt.subplots(1, 4, figsize=(30, 5))
     axs_pos = {'train_loss': (0),
                'train_acc': (1),
-               'test_loss': (2),
-               'test_acc': (3)}
+               'test_loss': (0),
+               'test_acc': (1)}
 
     for i in data:
         ax = axs[axs_pos[i]]
@@ -62,7 +62,30 @@ def show_misclassified_images(test_images, target_labels, target_predictions, nr
     fig.suptitle('Misclassified Images in GBN Model')
 
     for ax, image, target, prediction in zip(axes.flatten(), test_images, target_labels, target_predictions):
-        ax.imshow(image[0])
+        ax.imshow(np.uint8(torch.Tensor.cpu(image[0].permute(2, 1, 0))))
         ax.set(title='target:{t} prediction:{p}'.format(
             t=target.item(), p=prediction.item()))
         ax.axis('off')
+
+
+def show_images_predictions(test_images, target_labels, target_predictions, classes, nrow=5, ncol=5, fig_size=(3, 3)):
+    fig, axes = plt.subplots(nrows=nrow, ncols=ncol, figsize=fig_size)
+    fig.subplots_adjust(hspace=0.5)
+    fig.suptitle('Misclassified Images in Model')
+    for ax, image, target, prediction in zip(axes.flatten(), test_images, target_labels, target_predictions):
+        ax.imshow(np.uint8(torch.Tensor.cpu(image[0].permute(2, 1, 0))))
+        ax.set(title='target:{t} prediction:{p}'.format(
+            t=classes[target.item()], p=classes[prediction.item()]))
+        ax.axis('off')
+
+
+def denormalize(tensor, mean, std):
+    if not tensor.ndimension() == 4:
+        raise TypeError('tensor should be 4D')
+
+    mean = torch.FloatTensor(mean).view(
+        1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+    std = torch.FloatTensor(std).view(
+        1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+
+    return tensor.mul(std).add(mean)
